1c8a4975e754d4fdd1a9478ca7277cb058e2d588
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2021-02-16 11:40:11 -0800

    Updated product search

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -3,6 +3,16 @@ import { Product } from "..";
 
 const ProductList = ({ products, compare, nameSearch }) => {
   const renderProducts = () => {
+    if (nameSearch /* && nameSearch.length > 2 */ ) {
+      // nameSearch = nameSearch.toLowerCase();
+      // return products.filter((product) => 
+      //   product.name.toLowerCase().includes(nameSearch)
+      // )
+      return products.filter((product) => {
+        const price = +product.price.slice(1);
+        return price < nameSearch;
+      });
+    }
     return products;
   };
 
